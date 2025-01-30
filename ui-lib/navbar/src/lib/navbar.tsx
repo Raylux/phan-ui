fc9339f785512f18d3b66e1a37f64c7b91eb90a8
Raylux <alessandro.phan.12@gmail.com>
2025-01-30 17:07:54 +0100

    feat: implemented barebone navbar component

--- ui-lib/navbar/src/lib/navbar.tsx
+++ ui-lib/navbar/src/lib/navbar.tsx
@@ -1,9 +1,24 @@
+import { ReactNode } from 'react';
 import styles from './navbar.module.css';
 
-export function Navbar() {
+type NavbarProps = {
+  title?: string;
+  textColor?: string;
+  bgColor?: string;
+  children?: ReactNode;
+}
+
+export const Navbar = ({title, textColor = "white", bgColor = "black", children}: NavbarProps) => {
+
+  const navbarStyle = (textColor: string, bgColor: string) => ({
+    color: textColor,
+    backgroundColor: bgColor,
+  });
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Navbar!</h1>
+    <div className={styles['navbar']} style={navbarStyle(textColor, bgColor)}>
+      <div>{title}</div>
+      <div>{children}</div>
     </div>
   );
 }
