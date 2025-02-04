6352df28b12032b86aaf118e5b74e84a8de2726b
Alessandro Phan <alessandro.phan.12@gmail.com>
2025-02-04 09:43:21 +0100

    Barebone proto navbar (#14)
    * init: initialized navbar library
    
    * feat: implemented barebone navbar component
    
    * Feat: Navbar docs

---
+++ ui-lib/navbar/README.md
@@ -0,0 +1,19 @@
+# Navbar
+Used to trigger an action or an event
+
+## Setup
+
+Run the following command to add the `navbar` component to your project
+
+```sh
+npm install @phan-ui/navbar
+```
+
+## Usage
+```jsx
+import { Navbar } from '@phan-ui/navbar'
+
+<Navbar>I'm a navbar</Navbar>
+```
+
+
---
+++ ui-lib/navbar/src/lib/navbar.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import styles from './navbar.module.css';
+
+
+type NavbarProps = {
+  title?: string;
+  textColor?: string;
+  bgColor?: string;
+  children?: ReactNode;
+}
+
+export const Navbar = ({title, textColor = "white", bgColor = "black", children}: NavbarProps) => {
+
+  const navbarStyle = (textColor: string, bgColor: string) => ({
+    color: textColor,
+    backgroundColor: bgColor,
+  });
+
+  return (
+    <div className={styles['navbar']} style={navbarStyle(textColor, bgColor)}>
+      <div>{title}</div>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export default Navbar;
