d91c0c7edf083e62076a74b7044edc0c4005c979
Raylux <alessandro.phan.12@gmail.com>
2025-02-04 10:50:35 +0100

    feat: proto grid component implemented

--- docs-app/src/app/app.tsx
+++ docs-app/src/app/app.tsx
@@ -1,9 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Grid } from '@phan-ui/grid';
 import { Header } from '@phan-ui/header';
 
 export function App() {
   return (
-    <Header size='sm'>Questa è una heading</Header>
+    <Grid columns={4} rowGap={20} columnGap={100}>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+      <div style={{border: "2px solid"}}> prova </div>
+    </Grid>
   );
 }
 
