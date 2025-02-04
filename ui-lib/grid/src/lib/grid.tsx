bd4c8939d1aed436642100ca9adcc0b0a6861c83
Raylux <alessandro.phan.12@gmail.com>
2025-02-04 10:50:32 +0100

    feat: proto grid component

--- ui-lib/grid/src/lib/grid.tsx
+++ ui-lib/grid/src/lib/grid.tsx
@@ -1,9 +1,28 @@
 import styles from './grid.module.css';
 
-export function Grid() {
+//TODO implement the possibility to insert gap as prop (mutually exclusive with columnGap and rowGap)
+type GridProps = {
+  columns: number;
+  justifyItems?: "start" | "center" | "end" | "stretch"; 
+  alignItems?: "start" | "center" | "end" | "stretch"; 
+  columnGap?: number;
+  rowGap?: number;
+  children?: React.ReactNode;
+}
+
+export const Grid = ({columns, justifyItems = "start", alignItems = "start", columnGap = 12, rowGap = 12, children}: GridProps) => {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Grid!</h1>
+    <div className={styles['grid']} 
+    style={
+      {
+        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        justifyItems: justifyItems,
+        alignItems: alignItems,
+        columnGap: `${columnGap}px`,
+        rowGap: `${rowGap}px`,
+      }
+    }>
+      {children}
     </div>
   );
 }
